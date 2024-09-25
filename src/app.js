2e62eea73c60ff8fa34ab08e83a8821b5eeb9de1
chefdude99 <chefdude.1999@gmail.com>
2024-09-25 03:50:06 +0000

    Listo el proyecto

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [" Dalila", " Akira", " Turron", " William"];
+let action = [" escupio", " comio", " vomito", " golpeo"];
+let what = [" el carro", " el play", " la cocina", " el telefono"];
+let when = [
+  " mientras gabriela dormia",
+  " durante el almuerzo",
+  " antes de irse",
+  " luego de clases",
+  " anoche en la fiesta"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = Math.floor(Math.random() * 4);
+  let randomAction = Math.floor(Math.random() * 4);
+  let randomWhat = Math.floor(Math.random() * 4);
+  let randomWhen = Math.floor(Math.random() * 4);
+  document.getElementById("who").innerHTML =
+    "<span>" + who[randomWho] + "</span>";
+  document.getElementById("action").innerHTML =
+    "<span>" + action[randomAction] + "</span>";
+  document.getElementById("what").innerHTML =
+    "<span>" + what[randomWhat] + "</span>";
+  document.getElementById("when").innerHTML =
+    "<span>" + when[randomWhen] + "</span>";
 };
